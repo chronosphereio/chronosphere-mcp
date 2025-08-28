d3e158dcc4ddcfc2fce468e14463787805b7a9a2
Zach Cross <zcross@chronosphere.io>
2025-08-28 11:32:21 -0400

    chore: add analyze alerts endpoint for tool usage

--- generated/dataunstable/dataunstable/data_unstable/data_unstable_client.go
+++ generated/dataunstable/dataunstable/data_unstable/data_unstable_client.go
@@ -54,6 +54,8 @@ type ClientOption func(*runtime.ClientOperation)
 
 // ClientService is the interface for Client methods
 type ClientService interface {
+	AnalyzeAlerts(params *AnalyzeAlertsParams, opts ...ClientOption) (*AnalyzeAlertsOK, error)
+
 	Autocomplete(params *AutocompleteParams, opts ...ClientOption) (*AutocompleteOK, error)
 
 	CancelLogQuery(params *CancelLogQueryParams, opts ...ClientOption) (*CancelLogQueryOK, error)
@@ -117,6 +119,43 @@ type ClientService interface {
 	StartRangeQuery(params *StartRangeQueryParams, opts ...ClientOption) (*StartRangeQueryOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
+}
+
+/*
+AnalyzeAlerts alerts
+*/
+func (a *Client) AnalyzeAlerts(params *AnalyzeAlertsParams, opts ...ClientOption) (*AnalyzeAlertsOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewAnalyzeAlertsParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "AnalyzeAlerts",
+		Method:             "POST",
+		PathPattern:        "/api/unstable/data/alerts:analyze",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &AnalyzeAlertsReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*AnalyzeAlertsOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*AnalyzeAlertsDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
 /*
